b123a7bc3b9f6fdcea23d859b90223426bde7558
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-08-29 15:18:04 +0500

    Catalog View test added

--- elibrary_app/tests.py
+++ elibrary_app/tests.py
@@ -8,6 +8,33 @@ from .views import home
 from .forms import AddBookForm
 
 
+class CatalogViewTests(TestCase):
+    '''Тест для представлений'''
+
+    def test_book_list_view(self):
+        Book_1 = Catalog.objects.create(
+            title='Django for Beginners (2018)',
+            ISBN='978-1-60309-0',
+            author='John Doe',
+            price=9.99,
+            availbaility=True
+        )
+
+        Book_2 = Catalog.objects.create(
+            title='Django for Professionals (2020)',
+            ISBN='978-1-60309-3',
+            author='Mary Doe',
+            price=11.99,
+            availbaility=False
+        )
+
+        response = self.client.get(reverse('home'))
+
+        self.assertIn('Django for Professionals (2020)', response.content.decode())
+        self.assertIn('John Doe', response.content.decode())
+        self.assertIn('978-1-60309-3', response.content.decode())
+
+
 class CatalogTemplateTests(SimpleTestCase):
     '''Тест шаблона'''
 
