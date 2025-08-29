3a57287f48b8eb2cbd380fb5a8a73ece536ccb0b
TwoThousand07 <aytimov.erzhanae@gmail.com>
2025-08-29 14:57:46 +0500

    My first changes!

---
+++ elibrary_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from .forms import AddBookForm
+
+def home(request):  
+    add_book_form = AddBookForm()
+
+    return render(request, 'index.html', {
+        'add_book_form': add_book_form,
+    })
